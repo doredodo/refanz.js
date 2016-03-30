ad8a795884ee9e97fc3f67bfef7351131ec3c511
doredo <masnjuk729@gmail.com>
2016-03-30 13:14:31 +0700

    Create refanz.js

---
+++ refanz.js
@@ -0,0 +1,14 @@
+<h2> Follow Developers</h2>
+<table><tr><td>
+<center>
+<div class="rmenu" style="width: 50px">
+<a href="http://www.facebook.com/100006646245308" target="_blank"><img src="https://graph.facebook.com/100006646245308/picture?type=normal" width="50" height="50" class="founder-img" /> </a>
+<iframe align="center" src="//www.facebook.com/plugins/subscribe.php?href=https%3A%2F%2Fwww.facebook.com%2F100006646245308&amp;layout=box_count&amp;show_faces=false&amp;colorscheme=light&amp;font&amp;width=75&amp;appId=231341246993923" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:68px;" allowTransparency="true"></iframe>
+<td>
+<div class="rmenu" style="width: 50px">
+<a href="http://www.facebook.com/100002946681774" target="_blank"><img src="https://graph.facebook.com/100002946681774/picture?type=normal" width="50" height="50" class="founder-img" /> </a>
+<iframe align="center" src="//www.facebook.com/plugins/subscribe.php?href=https%3A%2F%2Fwww.facebook.com%2Fafingf1&amp;layout=box_count&amp;show_faces=false&amp;colorscheme=light&amp;font&amp;width=75&amp;appId=231341246993923" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:68px;" allowTransparency="true"></iframe>
+<td></div>
+<div class="menu">
+<div class="menu">
+<center>
